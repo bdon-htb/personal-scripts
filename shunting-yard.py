53dda0fafeefe4b501323a7c1351c3215d5e57cf
Brandon Phillips <46268307+bdon-htb@users.noreply.github.com>
2020-04-15 21:33:54 -0400

    Add files via upload
    wip of an implementation of a command line calculate.

---
+++ shunting-yard.py
@@ -0,0 +1,65 @@
+# Dictionary format: Operand character: Precendence value
+# The higher the value, the more precendence it has.
+OPERANDS = {'+':0,
+            '-':0,
+            '*':1,
+            '/':1}
+operator_stack = []
+output = ''
+
+def extract_data(s: str) -> list:
+    """Return a list of all items in the expression.
+    Multi-digit numbers will be accounted for as one item.
+    """
+    s = s.replace(' ','')
+    L = []
+    start = 0
+    for index in range(len(s)):
+        if s[index] in OPERANDS:
+            L.append(s[start:index])
+            L.append(s[index])
+            if index + 1 > len(s): # This should never be the case unless theres a user error.
+                start = index
+            else:
+                start = index + 1
+
+        if index == len(s) - 1:
+            L.append(s[start:len(s)])
+    return L
+
+
+def shunting_yard(s: list) -> str:
+    """Convert an index notation mathematical expression to reverse polish
+    notation.
+    """
+    output = ''
+    for c in s:
+        if c.isdigit():
+            output += ' ' + c
+        elif c in OPERANDS:
+            if operator_stack and OPERANDS[c] <= OPERANDS[operator_stack[-1]]:
+                output += ' ' + operator_stack.pop()
+            operator_stack.append(c)
+
+    while operator_stack:
+        output += ' ' + operator_stack.pop()
+
+    return output.strip()
+
+def calculate(s: str) -> float:
+    """Calculate an expression in reverse polish notation.
+    """
+    # TODO: Implement.
+    s = s.split(' ')
+    for index in range(len(s)):
+        if s[index] in OPERANDS:
+            pass
+    return
+
+def main():
+    s = input('Expression: ')
+    s = extract_data(s)
+    print(shunting_yard(s))
+
+if __name__ == '__main__':
+    main()

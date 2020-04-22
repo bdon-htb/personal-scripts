0ea4322e9fc270bb31e5961ea55e48526f4a2a28
Brandon Phillips <46268307+bdon-htb@users.noreply.github.com>
2020-04-22 01:10:46 -0400

    Update and rename shunting-yard.py to shunting_yard.py
    Added parenthesis support

--- shunting_yard.py
+++ shunting_yard.py
@@ -4,6 +4,9 @@ OPERANDS = {'+':0,
             '-':0,
             '*':1,
             '/':1}
+
+PARENTHESIS = ['(', ')']
+
 operator_stack = []
 output = ''
 
@@ -11,11 +14,18 @@ def extract_data(s: str) -> list:
     """Return a list of all items in the expression.
     Multi-digit numbers will be accounted for as one item.
     """
+    # TODO: Investigate and remove redundancies
     s = s.replace(' ','')
     L = []
     start = 0
     for index in range(len(s)):
-        if s[index] in OPERANDS:
+        if s[index] in PARENTHESIS:
+            L.append(s[index])
+            if index + 1 > len(s):
+                start = index
+            else:
+                start = index + 1
+        elif s[index] in OPERANDS:
             L.append(s[start:index])
             L.append(s[index])
             if index + 1 > len(s): # This should never be the case unless theres a user error.
@@ -31,15 +41,25 @@ def extract_data(s: str) -> list:
 def shunting_yard(s: list) -> str:
     """Convert an index notation mathematical expression to reverse polish
     notation.
+
+    Precondition: s is already balanced and correctly formatted.
     """
     output = ''
     for c in s:
         if c.isdigit():
             output += ' ' + c
         elif c in OPERANDS:
-            if operator_stack and OPERANDS[c] <= OPERANDS[operator_stack[-1]]:
+            if operator_stack and not (operator_stack[-1] in PARENTHESIS) and \
+                OPERANDS[c] <= OPERANDS[operator_stack[-1]]:
                 output += ' ' + operator_stack.pop()
             operator_stack.append(c)
+        elif c == '(':
+            operator_stack.append(c)
+        elif c == ')':
+            while operator_stack[-1] != '(' and len(operator_stack) > 0:
+                output += ' ' + operator_stack.pop()
+            if operator_stack[-1] == '(':
+                operator_stack.pop()
 
     while operator_stack:
         output += ' ' + operator_stack.pop()
@@ -59,6 +79,7 @@ def calculate(s: str) -> float:
 def main():
     s = input('Expression: ')
     s = extract_data(s)
+    print(s)
     print(shunting_yard(s))
 
 if __name__ == '__main__':

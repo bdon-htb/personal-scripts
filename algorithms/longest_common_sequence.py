7e2ff2af1317d25ddae720df1c63595f450ba9eb
bdon-htb <46268307+bdon-htb@users.noreply.github.com>
2020-08-21 23:40:45 -0400

    Added lcs method.

--- algorithms/longest_common_sequence.py
+++ algorithms/longest_common_sequence.py
@@ -16,6 +16,24 @@ def lcs_length(s1: str, s2: str) -> int:
     else: # If the last characters are not equal, compare the lcs between the other characters in string.
         return max(lcs_length(s1, s2[:-1]), lcs_length(s1[:-1], s2))
 
+def lcs(s1: str, s2: str, O='') -> str:
+    """Find the longest common subsequence between s1 and s2.
+    >>> lcs("BATD", "ABDACD")
+    'BAD'
+    >>> lcs("BANANA", "BANANA")
+    'BANANA'
+    >>> lcs("", "afmgsg")
+    ''
+    """
+    if len(s1) == 0 or len(s2) == 0:
+        return O
+    elif s1[-1] == s2[-1]:
+        return lcs(s1[:-1], s2[:-1], s1[-1] + O)
+    elif lcs_length(s1[:-1], s2) > lcs_length(s1, s2[:-1]):
+        return lcs(s1[:-1], s2, O)
+    else:
+        return lcs(s1, s2[:-1], O)
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()

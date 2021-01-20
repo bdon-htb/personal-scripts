1f0caf36a795d32b9637bbab864ada41cb0520f0
bdon-htb <brandonphillips2001@gmail.com>
2021-01-20 00:44:12 -0500

    Implement insert

--- algorithms/python_tree/python_tree2.py
+++ algorithms/python_tree/python_tree2.py
@@ -27,8 +27,9 @@ class CodeTreeNode:
         return self.nodes
 
     def add_node(self, node):
-        """Precondition: A node of the same name does not already exist in
-        self.contents
+        """Add node to the CURRENT node.
+
+        Precondition: The name of node is not in self.contents
         """
         self.content.append(node)
         self.nodes[node.name] = node
@@ -36,27 +37,66 @@ class CodeTreeNode:
     def add_content(self, c):
         """Add c to self.content. Accepts both CodeTreeNode and lines of code (str)
         """
-        if isinstance(obj, CodeTreeNode):
-            self.add_node(obj)
+        if isinstance(c, CodeTreeNode):
+            self.add_node(c)
         else: # Assume c is a string.
             self.content.append(c)
 
     def get_node(self, url: str):
         """Get the node at the given url endpoint. If the endpoint
         doesn't exist return None
+
+        Precondition: url is a non-empty string.
         """
         url = url.split('/')
-        if self.name == url[-1]: # At endpoint
+        if len(url) == 1 and self.name == url[0]: # At endpoint
             return self
         elif len(url) > 1 and url[1] in self.nodes:
-            next_node = self.nodes[url[1]] # Set the next node
+            next_node = self.nodes[url[1]] # Set the next node to search.
             url = '/'.join(url[1:]) # Trim front of list and convert back to string.
             return next_node.get_node(url)
         else: # Can't find the endpoint in the current node.
             return None
 
+    def insert(self, url, item):
+        """Insert item into tree at the given url's endpoint.
+
+        Precondition: all nodes in url already exist.
+
+        Because this tree is designed to be lossless, cannot create nodes automatically
+        without their fullname
+        """
+        url = url.split('/')
+        if len(url) == 1 and self.name == url[0]: # At endpoint
+            self.add_content(item)
+        else: # Else; assume there's a next node
+            try:
+                next_node = self.nodes[url[1]] # Set the next node to search.
+            except KeyError:
+                raise Exception(f"Child node '{url[1]}' does not exist in parent node '{self.name}'")
+
+            url = '/'.join(url[1:]) # Trim front of list and convert back to string.
+            next_node.insert(url, item)
+
+# TODO: Implement.
+def make_code_tree(filename: str) -> CodeTreeNode:
+    """Create a CodeTree from a python file.
+
+    Precondition: file is a valid python file without errors.
+    """
+    url = filename
+    tree = CodeTreeNode(url)
+    last_indent = 0
+    indent_spacing = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            pass
+    return tree
+
 t = CodeTreeNode('file.py')
-n = CodeTreeNode('      class Bob:\n')
-t.add_node(n)
-x = t.get_node('file.py/Bob')
-print(x.get_node('file.py/Bob').name)
+t.insert('file.py', 'x = y')
+t.insert('file.py', CodeTreeNode('def test:'))
+t.insert('file.py/test', 'y = 2')
+print(t.get_node('file.py/test').get_content())
+print(t.get_content())
+t.insert('file.py/Eman', 'y = 2')
